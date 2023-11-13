95b2548ae0985714d10278a706ed7f622464d6f5
honsb91 <114128759+honsb91@users.noreply.github.com>
2023-11-13 13:05:28 +0900

    Android 수업 1일차
    .

---
+++ And01_HelloAndroid/app/src/main/java/com/example/and01_helloandroid/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.and01_helloandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String str = new String(); // ctrl + p = 파라메타 필요한거 찾을 때
+                                    // 코드 자동 완성  : alt + Insert // 생성자
+
+
+
+    }
+
+
+}
---
+++ And02_IntoLayoutConst/app/src/main/java/com/example/and02_intolayoutconst/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.and02_intolayoutconst;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
